95d92ab52837a9cc74ac21e3435719b63effa1d2
Alvaro Guzman <aagn2807@gmail.com>
2020-11-27 01:41:16 -0300

    created mock users and added chat-ui

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,6 +2,16 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
+
+
+        <q-btn
+          v-if="$route.fullPath.includes('/chat')"
+          flat
+          dense
+          v-go-back.single
+          icon='arrow_back'
+          label='back'/>
+
         <q-toolbar-title class="absolute-center">
            {{ title }}
         </q-toolbar-title>
--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -1,10 +1,39 @@
 <template>
-  <q-page class="flex">
+  <q-page class="flex column">
     <h5>Chat page</h5>
+    <q-footer elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-input
+            bg-color="white"
+            rounded
+            outlined
+            v-model="newMessage"
+            class="full-width"
+            label="Message"
+            dense
+          >
+            <template v-slot:after>
+              <q-btn
+                round
+                dense
+                flat
+                color="white"
+                icon="send" />
+            </template>
+          </q-input>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-page>
 </template>
 
 <script>
 export default {
-}
+  data(){
+    return {
+      newMessage: ''
+    }
+  }
+};
 </script>
