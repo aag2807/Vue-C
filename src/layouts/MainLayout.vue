3df8fb510bf3f66e6ea058594adc651bdfbaad92
Alvaro Guzman <aagn2807@gmail.com>
2020-11-26 23:40:19 -0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="absolute-center">
+           {{ title }}
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  computed:{
+    title(){
+      console.log()
+      let currentPath = this.$route.fullPath;
+      if (currentPath == '/') return 'Vue-c'
+      else if (currentPath == '/chat') return 'Chat'
+      else if (currentPath == '/auth') return 'Login'
+    }
+  }
+}
+</script>
---
+++ src/pages/Users.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-page class="flex q-pa-md">
+    <q-list class="full-width" separator>
+      <q-item
+        v-for="user in contacts"
+        to="/chat"
+        :key="user.id"
+        clickable
+        v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">
+            {{ user.name.charAt(0) }}
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge :color="user.online ? 'cyan-9' : 'red-4' ">
+            {{ user.online ? "Online" : "Offline" }}
+          </q-badge>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contacts: [
+        {
+          id: 1,
+          name: "Diana Medina",
+          online: true
+        },
+        {
+          id: 2,
+          name: "Alvaro Guzman",
+          online: false
+        },
+        {
+          id: 4,
+          name: "Evan You",
+          online: true
+        }
+      ]
+    };
+  }
+};
+</script>
